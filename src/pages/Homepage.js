eb4dc23f70377c78a433787b5f370dcdb404d554
Lesuz <50194165+Lesuz@users.noreply.github.com>
2021-04-30 19:12:35 +0300

    cleaned up code

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import './Homepage.css'
 
+// Component to display dinosaur info
 const Dino = (props) => {
   return (
     <div className="dinodata">
@@ -16,6 +17,7 @@ const Homepage = () => {
   const [dinos, setDinos] = useState([])
   const [filteredDinos, setFilteredDinos] = useState([])
 
+    // fetching data from API and saving it in the variable *dinos*
     useEffect(() => {
       fetch('https://dinosaursapi.herokuapp.com/api/getall')
         .then(result => {
@@ -26,13 +28,16 @@ const Homepage = () => {
         })
     }, [])
 
+    // saving input string into varibale *dinosaur* everytime there is a change
     const handleChange = (event) => {
       setDinosaur(event.target.value)
     }
 
+    // filter trought data and display wanted dinosaur
     useEffect(() => {
       let dinosCopy = [...dinos]
-      dinosCopy = dinosCopy.filter( dino => dino.dino.includes(dinosaur))
+      dinosCopy = dinosCopy.filter( dino => 
+        dino.dino.toLowerCase().includes(dinosaur.toLowerCase()))
       setFilteredDinos(dinosCopy)
     }, [dinosaur, dinos])
 
@@ -42,6 +47,7 @@ const Homepage = () => {
               <label>Which dinosaur are you looking for?</label>
               <input value={dinosaur} type='text' onChange={handleChange} />
             </form>
+              {/*Make as many components of dinosaur as there are in the filteredDino array, which can be all or the searched dinosaur */}
               {
                 filteredDinos.map(dino => 
                   <Dino key={dino.dino} name={dino.dino} info={dino.info}/>
